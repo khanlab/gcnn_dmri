10bdd45abdfce7d01ddb26970c308e51dd0d0ea4
Uzair Hussain <u2hussai@gra-login3.graham.sharcnet>
2022-08-02 16:20:18 -0400

    data handling folder added

---
+++ dataHandling/freesurfer_masks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sing=/project/6050199/akhanf/singularity/bids-apps/pwighton_freesurfer_7.1.0.sif
+
+export FC_LICENSE=export FS_LICENSE=/project/6050199/akhanf/opt/freesurfer/.license	
+singularity exec --bind /home/u2hussai/ $sing mri_binarize --i $1 --o $2/mask_all_wm.nii.gz --all-wm
+singularity exec --bind /home/u2hussai/ $sing mri_binarize --i $1 --o $2/mask_gm.nii.gz --gm
